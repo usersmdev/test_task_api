fafa310c205699eed760eba1c336d3e26014f409
pm@doka-it.ru <pm@doka-it.ru>
2023-12-06 12:40:52 +0300

    add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-feature/2.AddFavorites',
+        'version' => 'dev-feature/2.AddFavorites',
+        'reference' => 'cf2c56063984bf2e47bf102f51b9c19dbe3abb30',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-feature/2.AddFavorites',
+            'version' => 'dev-feature/2.AddFavorites',
+            'reference' => 'cf2c56063984bf2e47bf102f51b9c19dbe3abb30',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
